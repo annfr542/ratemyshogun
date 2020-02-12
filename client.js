2fa1b25950b62727d1f188e1799ce2691030ae2f
Oskar Löwek <oskarlowek@c-10-253-202-133.clients.eduroam.liu.se>
2020-02-12 14:03:15 +0100

    you can now comment as a character, no style or comment storage implemented

--- client.js
+++ client.js
@@ -10,7 +10,11 @@ function loadView(view=1){
     document.getElementById("toplist-button").classList.add('active');
   }
   else if (view == 3){
-    document.getElementById("current_view").innerHTML = document.getElementById("toplist_view").innerHTML;
+    document.getElementById("current_view").innerHTML = document.getElementById("quiz_view").innerHTML;
+    document.getElementById("quiz-button").classList.add('active');
+  }
+  else if (view == 4){
+    document.getElementById("current_view").innerHTML = document.getElementById("faq_view").innerHTML;
     document.getElementById("faq-button").classList.add('active');
   }
   else {
@@ -22,4 +26,20 @@ window.onload=loadView;
 
 function test(){
   window.alert("TEST_ALERT");
+}
+
+
+function post_comment(form){
+  character = form.character.value;
+  text = form.comment.value;
+  img = character + ".png";
+
+  html = "<div class=\"comment\">" +
+  "<div class=\"comment_image\">" +
+  "<img src=" + img + " class=\"commenter\">" +
+  "</div>" + "<div class=\"comment_text\">" +
+  "<p><b>" + String(character) + "</b><br>" +
+  "<em>" + String(text) + "</em></p>" + "</div>" + "</div>";
+
+  document.getElementById("comments").innerHTML += html;
 }
